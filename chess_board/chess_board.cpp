2d6527f04ee68d0380e37e172919f5ae3f3cf136
Mihai Varga <mihai.mv13@gmail.com>
2014-03-10 21:05:30 +0200

    vim :retab

--- chess_board/chess_board.cpp
+++ chess_board/chess_board.cpp
@@ -6,84 +6,84 @@
 using namespace std;
 
 void ChessBoard::initBoard() {
-	//initialize the board table
-	for (int i = 0; i < 12; i++) {
-		ChessBoard::boards[i] = 0;
-	}
-	//pawns
-	ChessBoard::boards[0] |= ChessBoard::maskRank[1];
-	ChessBoard::boards[6] |= ChessBoard::maskRank[6];
-	//rooks
-	ChessBoard::boards[1] |= ChessBoard::piece[0] | ChessBoard::piece[7];
-	ChessBoard::boards[7] |= ChessBoard::piece[63] | ChessBoard::piece[56];
-	//knights
-	ChessBoard::boards[2] |= ChessBoard::piece[1] | ChessBoard::piece[6];
-	ChessBoard::boards[8] |= ChessBoard::piece[62] | ChessBoard::piece[57];
-	//bishops
-	ChessBoard::boards[3] |= ChessBoard::piece[2] | ChessBoard::piece[5];
-	ChessBoard::boards[9] |= ChessBoard::piece[61] | ChessBoard::piece[58];
-	//queen
-	ChessBoard::boards[4] |= ChessBoard::piece[3];
-	ChessBoard::boards[10] |= ChessBoard::piece[59];
-	//king
-	ChessBoard::boards[5] |= ChessBoard::piece[4];
-	ChessBoard::boards[11] |= ChessBoard::piece[60];
-	//initialize global tables
-	ChessBoard::allBlacks = 0;
-	ChessBoard::allWhites = 0;
-	for (int i = 0; i < 6; i++) {
-		ChessBoard::allWhites |= ChessBoard::boards[i];
-		ChessBoard::allBlacks |= ChessBoard::boards[i + 6];
-	}
-	ChessBoard::allPieces = ChessBoard::allWhites | ChessBoard::allBlacks;
+    //initialize the board table
+    for (int i = 0; i < 12; i++) {
+        ChessBoard::boards[i] = 0;
+    }
+    //pawns
+    ChessBoard::boards[0] |= ChessBoard::maskRank[1];
+    ChessBoard::boards[6] |= ChessBoard::maskRank[6];
+    //rooks
+    ChessBoard::boards[1] |= ChessBoard::piece[0] | ChessBoard::piece[7];
+    ChessBoard::boards[7] |= ChessBoard::piece[63] | ChessBoard::piece[56];
+    //knights
+    ChessBoard::boards[2] |= ChessBoard::piece[1] | ChessBoard::piece[6];
+    ChessBoard::boards[8] |= ChessBoard::piece[62] | ChessBoard::piece[57];
+    //bishops
+    ChessBoard::boards[3] |= ChessBoard::piece[2] | ChessBoard::piece[5];
+    ChessBoard::boards[9] |= ChessBoard::piece[61] | ChessBoard::piece[58];
+    //queen
+    ChessBoard::boards[4] |= ChessBoard::piece[3];
+    ChessBoard::boards[10] |= ChessBoard::piece[59];
+    //king
+    ChessBoard::boards[5] |= ChessBoard::piece[4];
+    ChessBoard::boards[11] |= ChessBoard::piece[60];
+    //initialize global tables
+    ChessBoard::allBlacks = 0;
+    ChessBoard::allWhites = 0;
+    for (int i = 0; i < 6; i++) {
+        ChessBoard::allWhites |= ChessBoard::boards[i];
+        ChessBoard::allBlacks |= ChessBoard::boards[i + 6];
+    }
+    ChessBoard::allPieces = ChessBoard::allWhites | ChessBoard::allBlacks;
 }
 
 ChessBoard::ChessBoard() {
-	// initialize the masks
-	for (int i = 0; i < 8; i++) {
-		ChessBoard::maskRank[i] = 0;
-		ChessBoard::clearRank[i] = -1;
-		ChessBoard::maskFile[i] = 0;
-		ChessBoard::clearFile[i] = -1;
-		for (int j = 8 * i; j < 8 * i + 8; j++) {
-			ChessBoard::maskRank[i] |= 1LL << j;
-			ChessBoard::clearRank[i] &= ~(1LL << j);
-		}
-		for (int j = i; j < 64; j = j + 8) {
-			ChessBoard::maskFile[i] |= 1LL << j;
-			ChessBoard::clearFile[i] &= ~(1LL << j);
-		}
-	}
-	//initialize piece lookup table
-	for (int i = 0; i < 64; i++) {
-		ChessBoard::piece[i] = 0;
-		ChessBoard::piece[i] = 1LL << i;
-	}
-	ChessBoard::initBoard();
+    // initialize the masks
+    for (int i = 0; i < 8; i++) {
+        ChessBoard::maskRank[i] = 0;
+        ChessBoard::clearRank[i] = -1;
+        ChessBoard::maskFile[i] = 0;
+        ChessBoard::clearFile[i] = -1;
+        for (int j = 8 * i; j < 8 * i + 8; j++) {
+            ChessBoard::maskRank[i] |= 1LL << j;
+            ChessBoard::clearRank[i] &= ~(1LL << j);
+        }
+        for (int j = i; j < 64; j = j + 8) {
+            ChessBoard::maskFile[i] |= 1LL << j;
+            ChessBoard::clearFile[i] &= ~(1LL << j);
+        }
+    }
+    //initialize piece lookup table
+    for (int i = 0; i < 64; i++) {
+        ChessBoard::piece[i] = 0;
+        ChessBoard::piece[i] = 1LL << i;
+    }
+    ChessBoard::initBoard();
 }
 
 ChessBoard::~ChessBoard() {}
 
 void ChessBoard::printBoard(ChessBoard::Bitboard b) {
-	for (int i = 0; i < 64; i++) {
-		Bitboard c = b & (1LL << i);
-		if (c) {
-			cout << 1 << " ";
-		} else {
-			cout << 0 << " ";
-		}
-		if ((i + 1) % 8 == 0) {
-			cout << endl;
-		}
-	}
-	cout << endl;
+    for (int i = 0; i < 64; i++) {
+        Bitboard c = b & (1LL << i);
+        if (c) {
+            cout << 1 << " ";
+        } else {
+            cout << 0 << " ";
+        }
+        if ((i + 1) % 8 == 0) {
+            cout << endl;
+        }
+    }
+    cout << endl;
 }
 
 ChessBoard::Bitboard ChessBoard::getPiece(char c, int i) {
-	if (c > 'h' || c < 'a' || i > 8 || i < 1) throw 1;
-	int ind = c - 'a';
-	ind += (i - 1) * 8;
-	return ChessBoard::piece[ind];
+    if (c > 'h' || c < 'a' || i > 8 || i < 1) throw 1;
+    int ind = c - 'a';
+    ind += (i - 1) * 8;
+    return ChessBoard::piece[ind];
 }
 
 /*
@@ -92,38 +92,38 @@ ChessBoard::Bitboard ChessBoard::getPiece(char c, int i) {
  * 5 6 7
  */
 ChessBoard::Bitboard ChessBoard::getKingMoves(ChessBoard::Bitboard b) {
-	ChessBoard::Bitboard moves[8], ret;
-	// trimA and trimH check if the king is on
-	// file A or H respectively
-	ChessBoard::Bitboard trimA, trimH;
-	trimA = b & ChessBoard::clearFile[0];
-	trimH = b & ChessBoard::clearFile[7];
-	for (int i = 0; i < 8; i++) {
-		moves[i] = 0;
-	}
-	moves[0] = trimA >> 9;
-	moves[1] = b >> 8;
-	moves[2] = trimH >> 7;
-	moves[3] = trimA >> 1;
-	moves[4] = trimH << 1;
-	moves[5] = trimA << 7;
-	moves[6] = b << 8;
-	moves[7] = trimH << 9;
-	ret = 0;
-	for (int i = 0; i < 8; i++) {
-		ret |= moves[i];
-	}
-	// if moving the white king
-	if (b == ChessBoard::boards[5]) {
-		ret &= ~ChessBoard::allWhites;
-	} else {
-		if (b == ChessBoard::boards[11]) { // if moving the black king
-				ret &= ~ChessBoard::allBlacks;
-		} else {
-			throw 1;
-		}
-	}
-	return ret;
+    ChessBoard::Bitboard moves[8], ret;
+    // trimA and trimH check if the king is on
+    // file A or H respectively
+    ChessBoard::Bitboard trimA, trimH;
+    trimA = b & ChessBoard::clearFile[0];
+    trimH = b & ChessBoard::clearFile[7];
+    for (int i = 0; i < 8; i++) {
+        moves[i] = 0;
+    }
+    moves[0] = trimA >> 9;
+    moves[1] = b >> 8;
+    moves[2] = trimH >> 7;
+    moves[3] = trimA >> 1;
+    moves[4] = trimH << 1;
+    moves[5] = trimA << 7;
+    moves[6] = b << 8;
+    moves[7] = trimH << 9;
+    ret = 0;
+    for (int i = 0; i < 8; i++) {
+        ret |= moves[i];
+    }
+    // if moving the white king
+    if (b == ChessBoard::boards[5]) {
+        ret &= ~ChessBoard::allWhites;
+    } else {
+        if (b == ChessBoard::boards[11]) { // if moving the black king
+                ret &= ~ChessBoard::allBlacks;
+        } else {
+            throw 1;
+        }
+    }
+    return ret;
 }
 
 /*
@@ -134,40 +134,40 @@ ChessBoard::Bitboard ChessBoard::getKingMoves(ChessBoard::Bitboard b) {
  * - 6 - 5 -
  */
 ChessBoard::Bitboard ChessBoard::getKnightMoves(ChessBoard::Bitboard b) {
-	ChessBoard::Bitboard moves[8], ret;
-	// trimAB and trimGH check if the knight is on
-	// files (A, B) or (G, H) respectively
-	ChessBoard::Bitboard trimAB, trimGH, trimA, trimH;
-	trimAB = b & ChessBoard::clearFile[0] & ChessBoard::clearFile[1];
-	trimGH = b & ChessBoard::clearFile[7] & ChessBoard::clearFile[6];
-	trimA = b & ChessBoard::clearFile[0];
-	trimH = b & ChessBoard::clearFile[7];
-	for (int i = 0; i < 8; i++) {
-		moves[i] = 0;
-	}
-	moves[0] = trimAB >> 10;//
-	moves[1] = trimA >> 17;//
-	moves[2] = trimH >> 15;//
-	moves[3] = trimGH >> 6;
-	moves[4] = trimGH << 10;
-	moves[5] = trimH << 17;
-	moves[6] = trimA << 15;
-	moves[7] = trimAB << 6;//
-	ret = 0;
-	for (int i = 0; i < 8; i++) {
-		ret |= moves[i];
-	}
-	// if moving a white knight
-	if (b & ChessBoard::boards[2]) {
-		ret &= ~ChessBoard::allWhites;
-	} else {
-		if (b & ChessBoard::boards[8]) { // if moving a black knight
-				ret &= ~ChessBoard::allBlacks;
-		} else {
-			throw 1;
-		}
-	}
-	return ret;
+    ChessBoard::Bitboard moves[8], ret;
+    // trimAB and trimGH check if the knight is on
+    // files (A, B) or (G, H) respectively
+    ChessBoard::Bitboard trimAB, trimGH, trimA, trimH;
+    trimAB = b & ChessBoard::clearFile[0] & ChessBoard::clearFile[1];
+    trimGH = b & ChessBoard::clearFile[7] & ChessBoard::clearFile[6];
+    trimA = b & ChessBoard::clearFile[0];
+    trimH = b & ChessBoard::clearFile[7];
+    for (int i = 0; i < 8; i++) {
+        moves[i] = 0;
+    }
+    moves[0] = trimAB >> 10;//
+    moves[1] = trimA >> 17;//
+    moves[2] = trimH >> 15;//
+    moves[3] = trimGH >> 6;
+    moves[4] = trimGH << 10;
+    moves[5] = trimH << 17;
+    moves[6] = trimA << 15;
+    moves[7] = trimAB << 6;//
+    ret = 0;
+    for (int i = 0; i < 8; i++) {
+        ret |= moves[i];
+    }
+    // if moving a white knight
+    if (b & ChessBoard::boards[2]) {
+        ret &= ~ChessBoard::allWhites;
+    } else {
+        if (b & ChessBoard::boards[8]) { // if moving a black knight
+                ret &= ~ChessBoard::allBlacks;
+        } else {
+            throw 1;
+        }
+    }
+    return ret;
 }
 
 /*
@@ -177,14 +177,14 @@ ChessBoard::Bitboard ChessBoard::getKnightMoves(ChessBoard::Bitboard b) {
  * - 1 -
  */
 ChessBoard::Bitboard ChessBoard::getWhitePawnMoves(ChessBoard::Bitboard b) {
-	ChessBoard::Bitboard moves[2], ret;
-	moves[0] = 0;
-	moves[1] = 0;
-	moves[0] = b << 8;
-	moves[1] = b << 16;
-	ret = moves[0] | moves[1];
-	ret &= ~ChessBoard::allWhites;
-	return ret;
+    ChessBoard::Bitboard moves[2], ret;
+    moves[0] = 0;
+    moves[1] = 0;
+    moves[0] = b << 8;
+    moves[1] = b << 16;
+    ret = moves[0] | moves[1];
+    ret &= ~ChessBoard::allWhites;
+    return ret;
 }
 
 /*
@@ -194,12 +194,12 @@ ChessBoard::Bitboard ChessBoard::getWhitePawnMoves(ChessBoard::Bitboard b) {
  * - - -
  */
 ChessBoard::Bitboard ChessBoard::getBlackPawnMoves(ChessBoard::Bitboard b) {
-	ChessBoard::Bitboard moves[2], ret;
-	moves[0] = 0;
-	moves[1] = 0;
-	moves[0] = b >> 8;
-	moves[1] = b >> 16;
-	ret = moves[0] | moves[1];
-	ret &= ~ChessBoard::allBlacks;
-	return ret;
+    ChessBoard::Bitboard moves[2], ret;
+    moves[0] = 0;
+    moves[1] = 0;
+    moves[0] = b >> 8;
+    moves[1] = b >> 16;
+    ret = moves[0] | moves[1];
+    ret &= ~ChessBoard::allBlacks;
+    return ret;
 }
--- chess_board/chess_board.h
+++ chess_board/chess_board.h
@@ -3,55 +3,55 @@
 #include <stdint.h>
 
 class ChessBoard {
-	public:
-		typedef unsigned long long Bitboard;
-	public:
-		/*
-		 *  0 - white pawns
-		 *  1 - white rooks
-		 *  2 - white knights
-		 *  3 - white bishops
-		 *  4 - white queen
-		 *  5 - white king
-		 *  __________________
-		 *
-		 *  6 - black pawns
-		 *  7 - black rooks
-		 *  8 - black knights
-		 *  9 - black bishops
-		 * 10 - black queen
-		 * 11 - black king
-		 */
-		Bitboard boards[12], allWhites, allBlacks, allPieces;
-		/*
-		 * rank = row
-		 * file = column
-		 */
-		Bitboard maskRank[8], clearRank[8], maskFile[8], clearFile[8];
-		/* 
-		 * A1 B1 C1 D1 E1 F1 G1 H1
-		 * A2 B2 C2 D2 E2 F2 G2 H2
-		 * A3 B3 C3 D3 E3 F3 G3 H3
-		 * A4 B4 C4 D4 E4 F4 G4 H4
-		 * A5 B5 C5 D5 E5 F5 G5 H5
-		 * A6 B6 C6 D6 E6 F6 G6 H6
-		 * A7 B7 C7 D7 E7 F7 G7 H7
-		 * A8 B8 C8 D8 E8 F8 G8 H8
-		 */
-		Bitboard piece[64];
+    public:
+        typedef unsigned long long Bitboard;
+    public:
+        /*
+         *  0 - white pawns
+         *  1 - white rooks
+         *  2 - white knights
+         *  3 - white bishops
+         *  4 - white queen
+         *  5 - white king
+         *  __________________
+         *
+         *  6 - black pawns
+         *  7 - black rooks
+         *  8 - black knights
+         *  9 - black bishops
+         * 10 - black queen
+         * 11 - black king
+         */
+        Bitboard boards[12], allWhites, allBlacks, allPieces;
+        /*
+         * rank = row
+         * file = column
+         */
+        Bitboard maskRank[8], clearRank[8], maskFile[8], clearFile[8];
+        /* 
+         * A1 B1 C1 D1 E1 F1 G1 H1
+         * A2 B2 C2 D2 E2 F2 G2 H2
+         * A3 B3 C3 D3 E3 F3 G3 H3
+         * A4 B4 C4 D4 E4 F4 G4 H4
+         * A5 B5 C5 D5 E5 F5 G5 H5
+         * A6 B6 C6 D6 E6 F6 G6 H6
+         * A7 B7 C7 D7 E7 F7 G7 H7
+         * A8 B8 C8 D8 E8 F8 G8 H8
+         */
+        Bitboard piece[64];
 
-	private:
-		void initBoard();
-	public:
-		ChessBoard();
-		~ChessBoard();
+    private:
+        void initBoard();
+    public:
+        ChessBoard();
+        ~ChessBoard();
 
-		void printBoard(Bitboard b);
-		Bitboard getPiece(char c, int i);
-		Bitboard getKingMoves(Bitboard b);
-		Bitboard getKnightMoves(Bitboard b);
-		Bitboard getWhitePawnMoves(Bitboard b);
-		Bitboard getBlackPawnMoves(Bitboard b);
+        void printBoard(Bitboard b);
+        Bitboard getPiece(char c, int i);
+        Bitboard getKingMoves(Bitboard b);
+        Bitboard getKnightMoves(Bitboard b);
+        Bitboard getWhitePawnMoves(Bitboard b);
+        Bitboard getBlackPawnMoves(Bitboard b);
 };
 
 #endif
