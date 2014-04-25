d6e17eb350e980c8b9ba0c47b1215afcde5523e5
mihaivolmer <mihaivolmer@gmail.com>
2014-04-25 03:54:24 +0300

    Fixed a few typos. Good night

--- minimax.h
+++ minimax.h
@@ -1,35 +1,33 @@
 #include "chess_board/chess_board.h"
 
 using namespace std;
-/**
+
+/*
  * Implementarea de negamax cu alpha-beta pruning
- * Intoarce o pereche <x, y> unde x este cel mai bun scor
+ * Intoarce o pereche <int, PairBB> unde x este cel mai bun scor
  * care poate fi obtinut de jucatorul aflat la mutare,
  * iar y este mutarea propriu-zisa
  */
-pair<int, PairBB > minimax_abeta(ChessBoard cb, player_t player, int depth, int alfa, int beta)
+pair<int, PairBB> minimax_abeta(ChessBoard cb, player_t player, int depth, int alfa, int beta)
 {
     PairBB bestMove(0ULL, 0ULL);
 
 	if(depth == 0)
 	{
-		//std::pair<int,Move> aux(cb.eval(player), bestMove);
-		//return aux;
         return make_pair(cb.evaluate(player), bestMove);
 	}
 
 	int score;
-    
     vector<PairBB> moves;
     cb.getAllMoves(moves);
     ChessBoard newCb;
-	
+
 	for(unsigned int i = 0; i < moves.size(); i++)
 	{
         newCb = cb;
         newCb.setMove(moves[i].first, moves[i].second);
-		
-        pair<int, PairBB> aux = minimax_abeta(newCb, player, 
+
+        pair<int, PairBB> aux = minimax_abeta(newCb, player,
                 depth - 1, -beta, -alfa);
 		score = -1 * aux.first;
 		if(score >= beta)
@@ -43,6 +41,5 @@ pair<int, PairBB > minimax_abeta(ChessBoard cb, player_t player, int depth, int 
 		}
 	}
 
-    //return std::pair<int, Move>(alfa, bestMove);
     return make_pair(alfa, bestMove);
 }
